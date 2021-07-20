cba1315d4afd0d60480684ced99a019b9db6de80
Alexander Bello rodriguez <abello-r@c1r8s6.42madrid.com>
2021-07-20 16:41:10 +0200

    Full update

--- new_group.js
+++ new_group.js
@@ -3,5 +3,11 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
 	{
 		// Change <div> BlackHoleDays
 		document.getElementById("bh-date").innerText = " Take it easy";
+
+		// Change coalition name
+		document.getElementsByTagName("span.coalition-sp").innerText = "Legion"
+
+		document.getElementById("title-selector").innerText = "Member of the Legion";
+
 	}
 });
