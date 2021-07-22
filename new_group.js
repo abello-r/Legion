b0620af0ae7c71d6a1f8404f42197cc916ed073c
Alexander Bello rodriguez <abello-r@c1r8s6.42madrid.com>
2021-07-22 20:28:25 +0200

    legion banner 1/2

--- new_group.js
+++ new_group.js
@@ -2,13 +2,12 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
 	if (request.signal == "ready")
 	{
 		// Change <div> BlackHoleDays
-		document.getElementById("bh-date").innerText = " Take it easy";
+		document.querySelector("#bh-date").innerText = " Take it easy";
 
 		// Change coalition name
-		document.getElementsByClassName("coalition-span").innerText = "Legion"
+		document.querySelector("span.coalition-span").innerHTML = "Legion"
 
 		// Add new Tittle
 		document.getElementById("title-selector").innerText = "Member of the Legion";
-
 	}
 });
